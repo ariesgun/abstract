e6f22fa48687f44a270f0536ac901e7c2a665cbb
Aries Gunawan <aries.thio@hotmail.com>
2024-06-15 18:56:26 +0200

    custom dex

--- framework/packages/standards/dex/src/lib.rs
+++ framework/packages/standards/dex/src/lib.rs
@@ -12,4 +12,4 @@ pub use abstract_adapter_utils::{coins_in_assets, cw_approve_msgs, Identify};
 pub use command::{DexCommand, Fee, FeeOnInput, Return, Spread};
 pub use error::DexError;
 
-pub const DEX_ADAPTER_ID: &str = "abstract:dex";
+pub const DEX_ADAPTER_ID: &str = "xenosgeck:dex";
